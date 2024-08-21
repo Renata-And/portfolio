afcd85d01eb4691f593340f92d3df486edfaede0
renata-and <renata.androsova@gmail.com>
2024-08-21 15:37:53 +0700

    Initialize project

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Header = () => {
+  return (
+    <StyledHeader>Header</StyledHeader>
+  )
+}
+
+const StyledHeader = styled.header`
+  background-color: #f8e8e8;
+`
