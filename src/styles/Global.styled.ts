a04c039a2ae0f28ba404e347b592a64607450201
renata-and <renata.androsova@gmail.com>
2024-08-22 16:22:33 +0700

    Global styles, created Container and Theme

---
+++ src/styles/Global.styled.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+import { theme } from "./Theme";
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+  font-family: 'Nunito', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    background-color: unset;
+    border: none;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+  header, section, footer {
+    background-color: ${theme.colors.background};
+  }
+
+`
---
+++ src/styles/Theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    background: '#f9faff',
+    accent: '#fdc435',
+    fontPrimary: '#25282b',
+    fontSecondary: '#828282',
+    fontTitle: '#333'
+  }
+}
