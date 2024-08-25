b2b1395ffdebd93c55f7026a2a7dd660f4143454
renata-and <renata.androsova@gmail.com>
2024-08-25 11:24:21 +0700

    Added styles to the Skills section

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
-import { SectionTitle } from '../../../components/SectionTitle'
-import { FlexWrapper } from '../../../components/FlexWrapper'
+import { SectionTitle } from '../../../components/SectionTitle.styled'
+import { FlexWrapper } from '../../../components/FlexWrapper.styled'
 import { Icon } from '../../../components/icon/Icon'
+import { Container } from '../../../components/Container.styled'
 
 const skillsItems = ['css', 'git', 'github', 'html', 'js', 'react', 'reactToolkit', 'sass', 'tailwind', 'vscode'];
 
 export const Skills = () => {
   return (
     <StyledSkills>
-      <SectionTitle>Skills</SectionTitle>
-      <FlexWrapper justify={'center'} gap={'10px'} wrap={'wrap'}>
-        {skillsItems.map((item) => {
-          return (
-            <Icon iconId={item} width={'131'} height={'131'} viewBox={'0 0 131 131'} />
-          )
-        })}
-      </FlexWrapper>
+      <Container>
+        <SectionTitle>Skills</SectionTitle>
+        <FlexWrapper justify={'center'} gap={'50px'} wrap={'wrap'}>
+          {skillsItems.map((item, i) => {
+            return (
+              <Icon key={i} iconId={item} width={'131'} height={'131'} viewBox={'0 0 131 131'} />
+            )
+          })}
+        </FlexWrapper>
+      </Container>
     </StyledSkills>
   )
 }
 
 const StyledSkills = styled.section`
-  min-height: 40vh;
-  border: 1px solid blueviolet;
+  outline: 1px solid blueviolet;
 `
