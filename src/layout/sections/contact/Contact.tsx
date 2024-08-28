2c0470a6c96ffd9e742f9be2f0685bbe26a20f36
renata-and <renata.androsova@gmail.com>
2024-08-28 12:57:17 +0700

    Added styles to the Contact section

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -2,42 +2,78 @@ import React from 'react'
 import styled from 'styled-components'
 import { SectionTitle } from '../../../components/SectionTitle.styled'
 import { Button } from '../../../components/Button.styled'
+import { theme } from '../../../styles/Theme'
+import { Container } from '../../../components/Container.styled'
 
 export const Contact = () => {
   return (
     <StyledContact>
-      <SectionTitle>Contact me</SectionTitle>
-      <StyledForm>
-        <StyledLabel>Name</StyledLabel>
-        <Field type={'text'} />
-        <StyledLabel>Email</StyledLabel>
-        <Field type={'email'} />
-        <StyledLabel>Message</StyledLabel>
-        <Field as='textarea' />
-        <Button type={'submit'} btnType={'primary'}>Send</Button>
-      </StyledForm>
+      <Container>
+        <SectionTitle>Contact me</SectionTitle>
+        <StyledForm>
+          <StyledLabel>
+            <span>Name</span>
+            <Field type={'text'} />
+          </StyledLabel>
+          <StyledLabel>
+            <span>Email</span>
+            <Field type={'email'} />
+          </StyledLabel>
+          <StyledLabel>
+            <span>Message</span>
+            <Field as='textarea' />
+          </StyledLabel>
+          <Button type={'submit'} btnType={'primary'}>Send</Button>
+        </StyledForm>
+      </Container>
     </StyledContact>
   )
 }
 
 const StyledContact = styled.section`
-  min-height: 70vh;
-  outline: 1px solid blueviolet;
+  textarea {
+    resize: none;
+    height: 160px;
+  }
+  /* outline: 1px solid blueviolet; */
 `
 
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 5px;
+  gap: 24px;
   max-width: 400px;
+  width: 100%;
   margin: 0 auto;
-  outline: 1px solid blueviolet;
+  padding-bottom: 64px;
+  /* outline: 1px solid blueviolet; */
+
+  & button {
+    align-self: flex-end;
+  }
 `
 
 const StyledLabel = styled.label`
-  
+  font-weight: 600;
+  font-size: 16px;
+  color: ${theme.colors.fontPrimary};
+
+  span {
+    display: inline-block;
+    margin-bottom: 8px;
+  }
 `
 
 const Field = styled.input`
+  width: 100%;
+  border: 1px solid ${theme.colors.borderColor};
+  border-radius: 8px;
+  height: 40px;
 
+  font-size: 16px;
+  font-family: 'Nunito', sans-serif;
+
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.borderColor};
+  }
 `
