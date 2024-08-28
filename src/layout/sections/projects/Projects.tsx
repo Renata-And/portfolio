19f4bcceb67f0b2e00b6a4a5fa91e2a247ec10e1
renata-and <renata.androsova@gmail.com>
2024-08-28 12:56:24 +0700

    Added styles to the Projects section

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -1,37 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
-import { SectionTitle } from '../../../components/SectionTitle'
+import { SectionTitle } from '../../../components/SectionTitle.styled'
 import { Project } from './project/Project'
+import { Container } from '../../../components/Container.styled'
 import imgProj1 from '../../../assets/images/projects/project1.jpeg'
 import imgProj2 from '../../../assets/images/projects/project2.jpeg'
 import imgProj3 from '../../../assets/images/projects/project3.jpeg'
 
+// const projectItems = [
+//   { title: 'Project Name', description: 'I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.', href: '#', src: { imgProj1 } },
+//   { title: 'Project Name', description: 'What was your role, your deliverables, if the project was personal, freelancing.', href: '#', src: { imgProj2 } },
+//   { title: 'Project Name', description: 'You can also add in this description the type of the project, if it was for web, mobile, electron.', href: '#', src: { imgProj3 } },
+// ];
+
 export const Projects = () => {
   return (
     <StyledProjects>
-      <SectionTitle>Projects</SectionTitle>
-      <Project
-        title={'Project Name'}
-        description={'I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.'}
-        href={'#'}
-        src={imgProj1} />
+      <Container>
+        <SectionTitle>Projects</SectionTitle>
+        <Project
+          title={'Project Name'}
+          description={'I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.'}
+          href={'#'}
+          src={imgProj1} />
 
-      <Project
-        title={'Project Name'}
-        description={'What was your role, your deliverables, if the project was personal, freelancing.'}
-        href={'#'}
-        src={imgProj2} />
+        <Project
+          title={'Project Name'}
+          description={'What was your role, your deliverables, if the project was personal, freelancing.'}
+          href={'#'}
+          src={imgProj2}
+          direction={'row-reverse'} />
 
-      <Project
-        title={'Project Name'}
-        description={'You can also add in this description the type of the project, if it was for web, mobile, electron.'}
-        href={'#'}
-        src={imgProj3} />
+        <Project
+          title={'Project Name'}
+          description={'You can also add in this description the type of the project, if it was for web, mobile, electron.'}
+          href={'#'}
+          src={imgProj3} />
+      </Container>
     </StyledProjects>
   )
 }
 
 const StyledProjects = styled.section`
-  min-height: 100vh;
-  border: 1px solid blueviolet;
+  /* outline: 1px solid blueviolet; */
 `
--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -1,50 +1,82 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Button } from '../../../../components/Button'
+import { theme } from '../../../../styles/Theme'
+import { FlexWrapper } from '../../../../components/FlexWrapper.styled'
 
 type ProjectPropsType = {
   title: string
   description: string
   href: string
   src: string
+  direction?: string
 }
 
 export const Project = (props: ProjectPropsType) => {
   return (
     <StyledProject>
-      <div>
-        <ProjectTitle>{props.title}</ProjectTitle>
-        <Description>{props.description}</Description>
-        <Button as='a' href={props.href}>View Project</Button>
-      </div>
-      <Image src={props.src} />
+      <FlexWrapper justify={'space-between'} align={'center'} direction={props.direction || 'row'}>
+        <Wrapper>
+          <ProjectTitle>{props.title}</ProjectTitle>
+          <Text>{props.description}</Text>
+          <Link>View Project</Link>
+        </Wrapper>
+        <Image src={props.src} />
+      </FlexWrapper>
     </StyledProject>
   )
 }
 
 const StyledProject = styled.div`
-  display: flex;
-  justify-content: space-between;
-  max-width: 900px;
+  max-width: 990px;
+  width: 100%;
   margin: 0 auto;
-  border: 1px solid blueviolet;
+  box-shadow: 0 6px 64px 0 rgba(112, 144, 176, 0.1);
+  border-radius: 24px;
+  /* outline: 1px solid blueviolet; */
+  overflow: hidden;
 
-  div {
-    max-width: 410px;
+  & + & {
+    margin-top: 80px;
   }
 `
 
+const Wrapper = styled.div`
+  width: 50%;
+  padding: 0 50px;
+`
+
 const ProjectTitle = styled.h3`
-  
+  font-family: "Playfair Display", sans-serif;
+  font-size: 40px;
+  line-height: 150%;
+  color: ${theme.colors.fontPrimary};
 `
 
-const Description = styled.p`
-  
+const Text = styled.p`
+  margin-top: 25px;
+  margin-bottom: 25px;
 `
 
+const Link = styled.a`
+  display: inline-block;
+  border: 1px solid ${theme.colors.fontPrimary};
+  border-radius: 24px;
+  padding: 8px 24px;
+
+  font-family: "Roboto", sans-serif;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 150%;
+  color: ${theme.colors.fontPrimary};
+
+  &:hover {
+    border: 1px solid ${theme.colors.accent};
+    background-color: ${theme.colors.accent};
+  }
+`
 const Image = styled.img`
-  max-width: 300px;
-  width: 100%;
-  height: 400px;
+  display: inline-block;
+  width: 50%;
+  height: 526px;
   object-fit: cover;
 `
