a315c02580380a72dd06b4199a303e773aa38d2b
renata-and <renata.androsova@gmail.com>
2024-08-28 12:51:47 +0700

    Added styles to the Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,42 +2,49 @@ import React from 'react'
 import styled from 'styled-components'
 import { Icon } from '../../components/icon/Icon'
 import { FlexWrapper } from '../../components/FlexWrapper.styled'
+import { theme } from '../../styles/Theme'
+import bgImg from '../../assets/images/footerImg.png'
+import { Container } from '../../components/Container.styled'
 
 export const Footer = () => {
   return (
     <StyledFooter>
-      <FlexWrapper direction={'column'} align={'center'}>
-        <SocialList>
-          <SocialItem>
-            <SocialLink href={'#'}>
-              <Icon iconId={'instagram'} width={'48'} height={'49'} viewBox={'0 0 48 49'} />
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink href={'#'}>
-              <Icon iconId={'linkedin'} width={'48'} height={'49'} viewBox={'0 0 48 49'} />
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink href={'#'}>
-              <Icon iconId={'email'} width={'48'} height={'49'} viewBox={'0 0 48 49'} />
-            </SocialLink>
-          </SocialItem>
-        </SocialList>
-        <Copyright>Madelyn Torff 2021 </Copyright>
-      </FlexWrapper>
+      <Container>
+        <FlexWrapper direction={'column'} align={'center'}>
+          <SocialList>
+            <SocialItem>
+              <SocialLink href={'#'}>
+                <Icon iconId={'instagram'} width={'48'} height={'49'} viewBox={'0 0 48 49'} />
+              </SocialLink>
+            </SocialItem>
+            <SocialItem>
+              <SocialLink href={'#'}>
+                <Icon iconId={'linkedin'} width={'48'} height={'49'} viewBox={'0 0 48 49'} />
+              </SocialLink>
+            </SocialItem>
+            <SocialItem>
+              <SocialLink href={'#'}>
+                <Icon iconId={'email'} width={'48'} height={'49'} viewBox={'0 0 48 49'} />
+              </SocialLink>
+            </SocialItem>
+          </SocialList>
+          <Copyright>Madelyn Torff 2021 </Copyright>
+        </FlexWrapper>
+        <Image src={bgImg} />
+      </Container>
     </StyledFooter>
   )
 }
 
 const StyledFooter = styled.footer`
-  min-height: 30vh;
-  outline: 1px solid blueviolet;
+  padding-bottom: 240px;
+  position: relative;
 `
 
 const SocialList = styled.ul`
   display: flex;
   gap: 30px;
+  margin-bottom: 32px;
 `
 
 const SocialItem = styled.li`
@@ -45,9 +52,23 @@ const SocialItem = styled.li`
 `
 
 const SocialLink = styled.a`
-  
+  display: inline-block;
+  color: ${theme.colors.fontPrimary};
+
+  &:hover {
+    opacity: 0.7;
+    transform: scale(1.1);
+  }
 `
 
 const Copyright = styled.small`
-  
+  font-size: 16px;
+  color: ${theme.colors.fontSecondary};
+`
+
+const Image = styled.img`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
 `
