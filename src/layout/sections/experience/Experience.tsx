4dc5eeb6fd5d96e49ff72ecb62a4fa1b79c58909
renata-and <renata.androsova@gmail.com>
2024-08-28 12:58:09 +0700

    Added styles to the Experience section

--- src/layout/sections/experience/Experience.tsx
+++ src/layout/sections/experience/Experience.tsx
@@ -3,21 +3,40 @@ import styled from 'styled-components'
 import { SectionTitle } from '../../../components/SectionTitle.styled'
 import { FlexWrapper } from '../../../components/FlexWrapper.styled'
 import { Workplace } from './workplace/Workplace'
+import { Container } from '../../../components/Container.styled'
+import { theme } from '../../../styles/Theme'
 
 export const Experience = () => {
   return (
     <StyledExperience>
       <SectionTitle>Experience</SectionTitle>
-      <FlexWrapper justify={'space-around'} align={'center'}>
-        <Workplace year={2022} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.'} />
-        <Workplace year={2023} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.'} />
-        <Workplace year={2024} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.'} />
-      </FlexWrapper>
+      <Container>
+        <FlexWrapper justify={'space-around'} align={'center'}>
+          <Workplace year={2022} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.'} />
+          <Workplace year={2023} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.'} />
+          <Workplace year={2024} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor t ut labore et dolore magna aliqua.'} />
+        </FlexWrapper>
+      </Container>
     </StyledExperience>
   )
 }
 
 const StyledExperience = styled.section`
-  min-height: 50vh;
-  outline: 1px solid blueviolet;
+  /* outline: 1px solid blueviolet; */
+
+  ${FlexWrapper} {
+    position: relative;
+
+    &::before {
+      content: '';
+      display: inline-block;
+      width: 67%;
+      height: 5px;
+      background-color: ${theme.colors.accent};
+      position: absolute;
+      left: 50%;
+      top: 30%;
+      transform: translateX(-50%);
+      }
+  }
 `
--- src/layout/sections/experience/workplace/Workplace.tsx
+++ src/layout/sections/experience/workplace/Workplace.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { theme } from '../../../../styles/Theme'
 
 type WorkplacePropsType = {
   year: number
@@ -9,20 +10,49 @@ type WorkplacePropsType = {
 export const Workplace = (props: WorkplacePropsType) => {
   return (
     <StyledWorkplace>
-      <Year>{props.year}</Year>
+      <YearWrapper><Year>{props.year}</Year></YearWrapper>
       <Text>{props.text}</Text>
     </StyledWorkplace>
   )
 }
 
 const StyledWorkplace = styled.div`
-  max-width: 30%;
-  outline: 1px solid blueviolet;
+  max-width: 25%;
+  text-align: center;
+  /* outline: 1px solid blueviolet; */
 `
 
 const Year = styled.span`
-  
+  display: inline-block;
+  margin-bottom: 40px;
+
+  font-family: "Playfair Display", sans-serif;
+  font-weight: bold;
+  font-size: 24px;
+
+  position: relative;
+`
+
+const YearWrapper = styled.div`
+  position: relative;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 15px;
+    height: 15px;
+    background-color: ${theme.colors.fontTitle};
+    border-radius: 50%;
+
+    position: absolute;
+    left: 50%;
+    top: 76%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+  }
+
 `
 
 const Text = styled.p`
+
 `
